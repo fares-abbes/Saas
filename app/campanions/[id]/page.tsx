a7e0ae2bb0d8f854157f3b74ab541ddfa1601d42
Fares Abbes <fares111abbes@gmail.com>
2025-08-12 10:35:26 +0100

    first commit

---
+++ app/campanions/[id]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CopnionSession = () => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default CopnionSession;
---
+++ app/campanions/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Companions = () => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default Companions;
