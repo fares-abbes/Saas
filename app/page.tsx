3206753888e683e227a0b65c86188b5887610395
Fares Abbes <fares111abbes@gmail.com>
2025-08-12 22:15:51 +0100

    implement card

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,44 @@
+import CompanionCard from '@/components/CompanionCard';
+import CompanionList from '@/components/CompanionList';
+import CTA from '@/components/CTA';
 import { Button } from '@/components/ui/button'
-import React from 'react'
 
 const Page = () => {
   return (
- <Button>Hello</Button>
-  )
+    <main className="">
+      <h1 className="text-2xl underline">Popular Companion</h1>
+      <section className="home-section">
+        <CompanionCard
+          id="123"
+          name="learn cryptography"
+          topic="cryptography"
+          subject="computer science"
+          duration={30}
+          color="#e5d0ff"
+        />
+        <CompanionCard
+          id="123"
+          name="learn cryptography"
+          topic="cryptography"
+          subject="computer science"
+          duration={30}
+          color="#ffda6e"
+        />
+        <CompanionCard
+          id="123"
+          name="learn cryptography"
+          topic="cryptography"
+          subject="computer science"
+          duration={30}
+          color="#BDE7FF"
+        />
+      </section>
+      <section className="home-section">
+        <CompanionList />
+        <CTA />
+      </section>
+    </main>
+  );
 }
 
 export default Page
